c3fdac12ec28b13e258d1719506f46d6416b97fc
Luis Arita <github@luisarita.com>
2017-06-29 09:52:37 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+	hash = {}
+	array.each do |element|
+		if !hash.has_key?(element)
+			hash[element] = 1
+		else
+			hash[element] += 1
+		end
+	end
+  hash
 end
  
